0c94285e891676a0c252a0b7d282f3a38d49c01a
kulvir <kulvirdhillon240@gmail.com>
2020-12-15 11:09:39 -0500

    search remedies view controller added

---
+++ DoorDoctor/View Controllers/SearchRemedieVC.swift
@@ -0,0 +1,128 @@
+//
+//  SearchRemedieVC.swift
+//  DoorDoctor
+//
+//  Created by Kulvir Dhillon on 2020-12-15.
+//  Copyright © 2020 Kulvir Dhillon. All rights reserved.
+//
+
+import UIKit
+
+class SearchRemedieVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    
+    @IBOutlet weak var searchRemd: UISearchBar!
+    
+    
+    @IBOutlet weak var learnlbl: UILabel!
+    
+    @IBOutlet weak var sButtonLbl: UIButton!
+    
+    @IBOutlet var cardTableView: UITableView!
+    
+    var addbtn : UIBarButtonItem!
+    
+    var webItem = -1
+    
+    let pictures: [UIImage] = [UIImage(named: "fever.jpg")!, UIImage(named: "headache.jpeg")! , UIImage(named: "CoughCold.png")!,UIImage(named: "Stomachache.jpg")!]
+    let titles: [String] = ["Fever","Headache", "ColdCough", "Stomachache"]
+    let descriptions: [String] = ["Fever is when a human's body temperature goes above the normal range of 36–37° Centigrade.","Headache is pain in any region of the head.This may occur on one or both sides of the head.", "ColdCough is a self-limited contagious disease that can be caused by different types of viruses.","A stomach ache is refer to cramps or a dull ache in the tummy .It's usually short-lived."]
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        Utilities.styleFilledButton(sButtonLbl)
+        self.navigationItem.title = "Search"
+        
+        let tapgesture = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
+        learnlbl.addGestureRecognizer(tapgesture)
+        
+    }
+    
+    @objc func viewTapped(){
+        searchRemd.resignFirstResponder()
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cardCell", for: indexPath) as! CardCell
+        
+        cell.configure(picture: pictures[indexPath.row], title: titles[indexPath.row], description: descriptions[indexPath.row])
+        
+        
+        
+        return cell
+    }
+    
+    
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        
+        if let cell = sender as? UITableViewCell{
+//            webItem = tableView.indexPath(for: cell)!.row
+            webItem = cardTableView.indexPath(for: cell)!.row
+        }
+        
+        if let play = segue.destination as? WebViewVC{
+           let u = WebUrl[webItem]
+            play.url = u
+           
+        }
+    }
+
+    
+    
+    @IBAction func searchBA(_ sender: Any) {
+        
+        let searchtext = searchRemd.text! + " remedie"
+        
+        fetchVideos(remediName: searchtext)
+        
+        
+    }
+    
+    @IBAction func userProfile(_ sender: UIBarButtonItem) {
+        
+        
+//        let vc = self.storyboard?.instantiateViewController(withIdentifier: "doctorListViewController") as? doctorListViewController
+//
+//        self.view.window?.makeKeyAndVisible()
+        if let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "doctorListViewController") as? infoViewController {
+            
+        
+                   if let navigator = navigationController {
+                       navigator.pushViewController(viewController, animated: true)
+                   }
+    }
+}
+    @IBAction func LogOut(_ sender: UIBarButtonItem) {
+     UserDefaults.standard.removeObject(forKey: "email")
+//        if let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "loginViewController") as? loginViewController {
+//            if let navigator = navigationController {
+//                navigator.pushViewController(viewController, animated: true)
+//            }
+//        }
+        
+       let vc = self.storyboard?.instantiateViewController(withIdentifier: "MainVC") as? ViewController
+
+                              self.view.window?.rootViewController = vc
+                              self.view.window?.makeKeyAndVisible()
+    }
+}
---
+++ DoorDoctor/View Controllers/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  DoorDoctor
+//
+//  Created by Kulvir Dhillon on 2020-12-15.
+//  Copyright © 2020 Kulvir Dhillon. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet var stack: UIStackView!
+    @IBOutlet var videoLayer: UIView!
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var LoginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setUpElements()
+        playVideo()
+    }
+    
+    
+    func setUpElements() {
+           
+           Utilities.styleFilledButton(signupButton)
+           Utilities.styleHollowButton(LoginButton)
+           
+       }
+     func playVideo() {
+            guard let path = Bundle.main.path(forResource: "Cowboy - 33318", ofType: "mp4") else {
+                return
+            }
+            
+            let player = AVPlayer(url: URL(fileURLWithPath: path))
+            let playerLayer = AVPlayerLayer(player: player)
+//            playerLayer.frame = self.view.bounds
+               playerLayer.frame = CGRect(x: 0, y: 0, width: 1000, height: 1000)
+            playerLayer.videoGravity = .resizeAspectFill
+            self.videoLayer.layer.addSublayer(playerLayer)
+            player.play()
+            videoLayer.bringSubviewToFront(signupButton)
+            videoLayer.bringSubviewToFront(LoginButton)
+            videoLayer.bringSubviewToFront(stack)
+            
+        }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        playVideo()
+    }
+
+}
+
