d3148470f6324bfa920b164fbdff3ee49f02d7b1
evneet <nancymaan4@gmail.com>
2020-12-14 19:19:34 -0500

    [ek] booking model added

---
+++ DoorDoctor/Model/BookingModal.swift
@@ -0,0 +1,31 @@
+//
+//  BookingModal.swift
+//  DoorDoctor
+//
+//  Created by Evneet kaur on 2020-12-14.
+//  Copyright © 2020 Kulvir Dhillon. All rights reserved.
+//
+
+import Foundation
+
+class BookingModal {
+    var dName: String
+    var uName: String
+    var Age: Int
+    var Gender: String
+    var Phone: Int
+    var uDate: String
+    var uTime: String
+    
+    
+    init(dName: String, uName: String , Age: Int, Gender: String,Phone: Int, uDate: String, uTime: String ) {
+        self.dName = dName
+        self.uName = uName
+        self.Age = Age
+        self.Gender = Gender
+        self.Phone = Phone
+        self.uDate = uDate
+        self.uTime = uTime
+    }
+}
+
