33c7a39d151f9f64d3f8bbc39015bb1ad82faa59
Amanpreet Kaur <aman021011@gmail.com>
2020-12-15 10:28:40 -0500

    Manager for MapView

---
+++ DoorDoctor/ManagersSwiftUI/LocalManager.swift
@@ -0,0 +1,45 @@
+//
+//  LocalManager.swift
+//  DoorDoctor
+//
+//  Created by Amanpreet Kaur on 2020-12-15.
+//  Copyright © 2020 Kulvir Dhillon. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class LocationManager: NSObject, ObservableObject{
+    
+    private let locationManager = CLLocationManager()
+    @Published var location: CLLocation? = nil
+    
+    override init() {
+        super.init()
+        
+        
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.distanceFilter = kCLDistanceFilterNone
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+        
+    }
+    
+}
+extension LocationManager: CLLocationManagerDelegate{
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print(status)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {
+            return
+        }
+        
+        self.location = location
+    }
+    
+    
+}
