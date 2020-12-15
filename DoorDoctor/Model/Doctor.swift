d66598d42548813a26ae4e8741ee40dd9833af7c
evneet <nancymaan4@gmail.com>
2020-12-14 19:11:30 -0500

    [ek] Model doctor added

---
+++ DoorDoctor/Model/Doctor.swift
@@ -0,0 +1,28 @@
+//
+//  Doctor.swift
+//  DoorDoctor
+//
+//  Created by Evneet kaur on 2020-12-14.
+//  Copyright © 2020 Kulvir Dhillon. All rights reserved.
+//
+
+import Foundation
+
+class Doctor{
+    
+    init(id: Int, name: String, specialist: String, phone: Int, email: String){
+        self.id = id
+        self.name = name
+        self.specialist = specialist
+        self.phone = phone
+        self.email = email
+    }
+    
+    var id: Int
+    var name: String
+    var specialist: String
+    var phone: Int
+    var email: String
+
+    static var doctor = [Doctor]()
+}
